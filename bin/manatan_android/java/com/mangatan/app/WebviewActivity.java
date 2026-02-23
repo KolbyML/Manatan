1de1c0bf0e2abe45f6ec0a6d0df6f99567bc2c47
sohilsayed <sohilsayed19@gmail.com>
2026-02-23 02:11:54 +0200

    add missing import

--- bin/manatan_android/java/com/mangatan/app/WebviewActivity.java
+++ bin/manatan_android/java/com/mangatan/app/WebviewActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
